738710d4413d5f4c0dd1b44caec5b8bd0b8b0e06
EnderMandS <952025764@qq.com>
2023-11-16 08:15:16 +0000

    add pid dynamic reconfigure

--- code/src/dev/flight_control/include/flight_control.h
+++ code/src/dev/flight_control/include/flight_control.h
@@ -2,15 +2,20 @@
 #define __FLIGHT_CONTROL_
 
 #include "ros/ros.h"
+#include <cstdint>
+#include <dynamic_reconfigure/server.h>
 
 #include "airsim_ros/RotorPWM.h"
+#include "pid.h"
 #include "quadrotor_msgs/PositionCommand.h"
+#include <flight_control/flight_pidConfig.h>
 
 class FlightControl {
 public:
   FlightControl(ros::NodeHandle &);
 
 private:
+  // ROS
   ros::NodeHandle &nh;
   ros::ServiceClient takeoff_client, land_client;
   ros::Publisher pwm_pub;
@@ -21,6 +26,14 @@ private:
 
   void pwmPubTimerCb(const ros::TimerEvent &);
   void posSubCb(const quadrotor_msgs::PositionCommandConstPtr &);
+
+  // PID
+  dynamic_reconfigure::Server<flight_pid::flight_pidConfig> dy_server;
+  dynamic_reconfigure::Server<flight_pid::flight_pidConfig>::CallbackType dy_cb_f;
+  PID::Pid pid_fl_pwm, pid_fr_pwm, pid_rl_pwm,
+      pid_rr_pwm; // front rear left right
+
+  void dyCb(flight_pid::flight_pidConfig &, uint32_t);
 };
 
 #endif
--- code/src/dev/flight_control/src/flight_control.cpp
+++ code/src/dev/flight_control/src/flight_control.cpp
@@ -2,6 +2,7 @@
 #include "airsim_ros/Land.h"
 #include "airsim_ros/RotorPWM.h"
 #include "airsim_ros/Takeoff.h"
+#include <boost/bind/bind.hpp>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "flight_control");
@@ -13,6 +14,7 @@ int main(int argc, char **argv) {
 }
 
 FlightControl::FlightControl(ros::NodeHandle &nh) : nh(nh) {
+  // ROS
   takeoff_client =
       nh.serviceClient<airsim_ros::Takeoff>("/airsim_node/drone_1/takeoff");
   land_client = nh.serviceClient<airsim_ros::Land>("/airsim_node/drone_1/land");
@@ -21,8 +23,22 @@ FlightControl::FlightControl(ros::NodeHandle &nh) : nh(nh) {
   pwm_pub_timer = nh.createTimer(ros::Duration(1.f / 200),
                                  &FlightControl::pwmPubTimerCb, this);
   pos_sub = nh.subscribe("/planning/pos_cmd", 10, &FlightControl::posSubCb, this);
+  dy_cb_f = boost::bind(&FlightControl::dyCb, this, _1, _2);
+  dy_server.setCallback(dy_cb_f);
+
+  // PID
+  
 }
 
-void FlightControl::pwmPubTimerCb(const ros::TimerEvent &e) {}
+void FlightControl::pwmPubTimerCb(const ros::TimerEvent &e) {
+  static float t_last = ros::Time::now().toSec();
+  float t_now = ros::Time::now().toSec();
+  float dt = t_now-t_last;
+  t_last = t_now;
+}
 
 void FlightControl::posSubCb(const quadrotor_msgs::PositionCommandConstPtr &msg) {}
+
+void FlightControl::dyCb(flight_pid::flight_pidConfig &cfg, uint32_t level) {
+  ROS_INFO("PID dynamic reconfigure update.");
+}
