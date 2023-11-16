0b94b2176ebaeb80fe976880d1f547bd83b1ca24
EnderMandS <952025764@qq.com>
2023-11-16 10:48:18 +0000

    add some pid param

--- code/src/dev/flight_control/include/flight_control.h
+++ code/src/dev/flight_control/include/flight_control.h
@@ -1,7 +1,9 @@
 #ifndef __FLIGHT_CONTROL_
 #define __FLIGHT_CONTROL_
 
+#include "geometry_msgs/PoseStamped.h"
 #include "ros/ros.h"
+#include <cmath>
 #include <cstdint>
 #include <dynamic_reconfigure/server.h>
 
@@ -9,6 +11,22 @@
 #include "pid.h"
 #include "quadrotor_msgs/PositionCommand.h"
 #include <flight_control/flight_pidConfig.h>
+
+class PidChain {
+public:
+  PidChain();
+  void positionSet(float x, float y, float z);
+  void velocitySet(float x, float y, float z);
+  void angleSet(float pitch, float yaw, float roll);
+  void positionUpdate(float t_now, float x_now, float y_now, float z_now);
+  
+  PID::Pid fl_pwm, fr_pwm, rl_pwm, rr_pwm; // front rear left right
+  PID::Pid angle_pitch, angle_yaw, angle_roll;
+  PID::Pid angle_vel_pitch, angle_vel_yaw, angle_vel_roll;
+  // PID::Pid acc_x, acc_y, acc_z;
+  PID::Pid vel_x, vel_y, vel_z;
+  PID::Pid position_x, position_y, position_z;
+};
 
 class FlightControl {
 public:
@@ -30,8 +48,7 @@ private:
   // PID
   dynamic_reconfigure::Server<flight_pid::flight_pidConfig> dy_server;
   dynamic_reconfigure::Server<flight_pid::flight_pidConfig>::CallbackType dy_cb_f;
-  PID::Pid pid_fl_pwm, pid_fr_pwm, pid_rl_pwm,
-      pid_rr_pwm; // front rear left right
+  PidChain pid_chain;
 
   void dyCb(flight_pid::flight_pidConfig &, uint32_t);
 };
--- code/src/dev/flight_control/src/flight_control.cpp
+++ code/src/dev/flight_control/src/flight_control.cpp
@@ -22,23 +22,70 @@ FlightControl::FlightControl(ros::NodeHandle &nh) : nh(nh) {
       nh.advertise<airsim_ros::RotorPWM>("/airsim_node/drone_1/rotor_pwm", 10);
   pwm_pub_timer = nh.createTimer(ros::Duration(1.f / 200),
                                  &FlightControl::pwmPubTimerCb, this);
-  pos_sub = nh.subscribe("/planning/pos_cmd", 10, &FlightControl::posSubCb, this);
+  pos_sub =
+      nh.subscribe("/planning/pos_cmd", 10, &FlightControl::posSubCb, this);
   dy_cb_f = boost::bind(&FlightControl::dyCb, this, _1, _2);
   dy_server.setCallback(dy_cb_f);
 
   // PID
-  
 }
 
-void FlightControl::pwmPubTimerCb(const ros::TimerEvent &e) {
-  static float t_last = ros::Time::now().toSec();
-  float t_now = ros::Time::now().toSec();
-  float dt = t_now-t_last;
-  t_last = t_now;
-}
+void FlightControl::pwmPubTimerCb(const ros::TimerEvent &e) {}
 
-void FlightControl::posSubCb(const quadrotor_msgs::PositionCommandConstPtr &msg) {}
+void FlightControl::posSubCb(
+  const quadrotor_msgs::PositionCommandConstPtr &msg) {
+  float t_now = msg->header.stamp.toSec();
+}
 
 void FlightControl::dyCb(flight_pid::flight_pidConfig &cfg, uint32_t level) {
   ROS_INFO("PID dynamic reconfigure update.");
+  pid_chain.fl_pwm.setPid(cfg.fl_pwm_p, cfg.fl_pwm_i, cfg.fl_pwm_d);
+  pid_chain.fr_pwm.setPid(cfg.fr_pwm_p, cfg.fr_pwm_i, cfg.fr_pwm_d);
+  pid_chain.rl_pwm.setPid(cfg.rl_pwm_p, cfg.rl_pwm_i, cfg.rl_pwm_d);
+  pid_chain.rr_pwm.setPid(cfg.rr_pwm_p, cfg.rr_pwm_i, cfg.rr_pwm_d);
+  pid_chain.angle_pitch.setPid(cfg.angle_pitch_p, cfg.angle_pitch_i,
+                               cfg.angle_pitch_d);
+  pid_chain.angle_yaw.setPid(cfg.angle_yaw_p, cfg.angle_yaw_i, cfg.angle_yaw_d);
+  pid_chain.angle_roll.setPid(cfg.angle_roll_p, cfg.angle_roll_i,
+                              cfg.angle_roll_d);
+  pid_chain.angle_vel_pitch.setPid(cfg.angle_vel_pitch_p, cfg.angle_vel_pitch_i,
+                                   cfg.angle_vel_pitch_d);
+  pid_chain.angle_vel_yaw.setPid(cfg.angle_vel_yaw_p, cfg.angle_vel_yaw_i,
+                                 cfg.angle_vel_yaw_d);
+  pid_chain.angle_vel_roll.setPid(cfg.angle_vel_roll_p, cfg.angle_vel_roll_i,
+                                  cfg.angle_vel_roll_d);
+  pid_chain.vel_x.setPid(cfg.vel_x_p, cfg.vel_x_i, cfg.vel_x_d);
+  pid_chain.vel_y.setPid(cfg.vel_y_p, cfg.vel_y_i, cfg.vel_y_d);
+  pid_chain.vel_z.setPid(cfg.vel_z_p, cfg.vel_z_i, cfg.vel_z_d);
+  pid_chain.position_x.setPid(cfg.position_x_p, cfg.position_x_i,
+                              cfg.position_x_d);
+  pid_chain.position_y.setPid(cfg.position_y_p, cfg.position_y_i,
+                              cfg.position_y_d);
+  pid_chain.position_z.setPid(cfg.position_z_p, cfg.position_z_i,
+                              cfg.position_z_d);
+}
+
+PidChain::PidChain() {}
+
+void PidChain::positionUpdate(float t_now, float x_now, float y_now, float z_now) {
+  vel_x.update(t_now, position_x.update(t_now, x_now));
+  vel_y.update(t_now, position_y.update(t_now, y_now));
+  vel_z.update(t_now, position_z.update(t_now, z_now));
+  // TODO
+}
+
+void PidChain::positionSet(float x, float y, float z) {
+  position_x.setExpect(x);
+  position_y.setExpect(y);
+  position_z.setExpect(z);
+}
+void PidChain::velocitySet(float x, float y, float z) {
+  vel_x.setExpect(x);
+  vel_y.setExpect(y);
+  vel_z.setExpect(z);
+}
+void PidChain::angleSet(float pitch, float yaw, float roll) {
+  angle_pitch.setExpect(pitch);
+  angle_yaw.setExpect(yaw);
+  angle_roll.setExpect(roll);
 }
