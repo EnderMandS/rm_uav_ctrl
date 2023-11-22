3665b31f9b64efcafedbdd211d050f94c46688c9
EnderMandS <952025764@qq.com>
2023-11-22 16:28:18 +0000

    add NavPath pub

--- code/src/dev/state_machine/include/state_machine.h
+++ code/src/dev/state_machine/include/state_machine.h
@@ -3,7 +3,7 @@
 
 #include "nav_msgs/Odometry.h"
 #include <airsim_ros/CirclePoses.h>
-#include <nav_msgs/Path.h>
+
 #include <ros/ros.h>
 
 class StateMachine {
@@ -16,11 +16,11 @@ private:
 
   int circle_now_index = 0;
   int waypoint_list[13]={0,1,2,3,4,5,6,7,12,13,14,15,16};
-  nav_msgs::Path waypoint_now;
   airsim_ros::CirclePoses circle_poses;
 
   void circlePoseCb(const airsim_ros::CirclePosesConstPtr &);
   void odomCb(const nav_msgs::OdometryConstPtr &);
+  void pubNavPath();
 };
 
 #endif
--- code/src/dev/state_machine/src/state_machine.cpp
+++ code/src/dev/state_machine/src/state_machine.cpp
@@ -1,4 +1,7 @@
 #include "state_machine.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include <nav_msgs/Path.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "state_machine");
@@ -15,11 +18,43 @@ StateMachine::StateMachine(ros::NodeHandle &nh) {
   waypoint_pub =
       nh.advertise<nav_msgs::Path>("/waypoint_generator/waypoints", 1);
 }
-
 void StateMachine::circlePoseCb(const airsim_ros::CirclePosesConstPtr &msg) {
-  circle_poses = *msg;
-  waypoint_pub.publish(msg->poses[waypoint_list[circle_now_index]]);
+  circle_poses.poses = msg->poses;
 }
 void StateMachine::odomCb(const nav_msgs::OdometryConstPtr &msg) {
-  
+#define DEAD_ABS 0.2
+  if (abs(msg->pose.pose.position.x -
+          circle_poses.poses[waypoint_list[circle_now_index]].position.x) <=
+          DEAD_ABS &&
+      abs(msg->pose.pose.position.y -
+          circle_poses.poses[waypoint_list[circle_now_index]].position.y) <=
+          DEAD_ABS &&
+      abs(msg->pose.pose.position.z -
+          circle_poses.poses[waypoint_list[circle_now_index]].position.z) <=
+          DEAD_ABS) {
+    ++circle_now_index;
+  }
+}
+void StateMachine::pubNavPath() {
+  nav_msgs::Path waypoint;
+  geometry_msgs::PoseStamped pose;
+
+  waypoint.header.frame_id = "world";
+  pose.header.frame_id = "world";
+  pose.pose.position.x = circle_poses.poses[waypoint_list[circle_now_index]].position.x;
+  pose.pose.position.y = circle_poses.poses[waypoint_list[circle_now_index]].position.y;
+  pose.pose.position.z = circle_poses.poses[waypoint_list[circle_now_index]].position.z;
+
+  tf2::Quaternion q;
+  q.setRPY(0, 0, circle_poses.poses[waypoint_list[circle_now_index]].yaw);
+  pose.pose.orientation.w = q.w();
+  pose.pose.orientation.x = q.x();
+  pose.pose.orientation.y = q.y();
+  pose.pose.orientation.z = q.z();
+
+  pose.header.stamp = ros::Time::now();
+  waypoint.poses.push_back(pose);
+  waypoint.header.stamp = ros::Time::now();
+
+  waypoint_pub.publish(waypoint);
 }
